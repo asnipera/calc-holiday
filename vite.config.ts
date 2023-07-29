23bec342233be8df5818681dbe479a82c060d453
李彦 <liyan@neotrident.com>
2023-07-29 19:03:33 +0800

    2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/calc-holiday/',
+  base: process.env.NODE_ENV === 'production' ? '/calc-holiday/' : '/',
   build: {
     outDir: 'docs'
   },
